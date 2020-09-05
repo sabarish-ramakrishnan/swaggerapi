3faac7bc113f3ed5efbd440ab66d96b176ed21e8
sabarish-ramakrishnan <sabarishr@hotmail.com>
2020-09-05 16:29:02 -0500

    using commaand palette

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Logging;
 
 namespace swaggerapi.Controllers
 {
-    //test commits
+    //test commits 1 more
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
